b867ba05baf455148e6dcfe94cd946e57a996398
RoccoD <130700555+RoccoccoR@users.noreply.github.com>
2023-07-25 12:01:22 +0200

    pushing-card-first-function

--- components/card/card.js
+++ components/card/card.js
@@ -1,1 +1,25 @@
-export function createCharacterCard() {}
+export function createCharacterCard() {
+  const li = document.createElement("li");
+  const cardTitle = document.createElement("h2");
+  const cardImg = document.createElement("img");
+  const statusDescription = document.createElement("dd");
+  const typeDescription = document.createElement("dd");
+  const occurrencesDescription = document.createElement("dd");
+
+  li.innerHTML = `
+    <div>
+      <img>src=${cardImg}</img>
+      <h2>${cardTitle}</h2>
+      <dt>Status</dt>
+      <dd>${statusDescription}</dd>
+      <dt>Type</dt>
+      <dd>${typeDescription}</dd>
+      <dt>Occurences</dt>
+      <dd>${occurrencesDescription}</dd>
+    </div>
+    `;
+
+  return li;
+}
+
+createCharacterCard();
