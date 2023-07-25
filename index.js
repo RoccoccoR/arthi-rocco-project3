6425d27f7d605a5f1dd8cffe108b4a0f1664429d
RoccoD <130700555+RoccoccoR@users.noreply.github.com>
2023-07-25 19:04:07 +0200

    a

--- index.js
+++ index.js
@@ -20,23 +20,28 @@ let searchQuery = "";
 fetchCharacters();
 
 nextButton.addEventListener("click", (event) => {
-  if (page === 42) {
+  page = page + 1;
+  if (page > maxPage) {
+    page = maxPage;
     event.preventDefault();
     return;
   }
-  page = page + 1;
-  // pagination.textContent = `${page} / ${maxPage}`;
+
   fetchCharacters();
+  pagination.textContent = `${page} / ${maxPage}`;
 });
 
 prevButton.addEventListener("click", (event) => {
-  if (page <= 1) {
+  page = page - 1;
+  if (page < 1) {
+    page = 1;
     event.preventDefault();
     return;
   }
-  page = page - 1;
-  // pagination.textContent = `${page} / ${maxPage}`;
+
   fetchCharacters();
+
+  pagination.textContent = `${page} / ${maxPage}`;
 });
 
 async function fetchCharacters() {
@@ -44,17 +49,19 @@ async function fetchCharacters() {
   const response = await fetch(apiUrl);
   const data = await response.json();
   if (data.error) {
-    cardContainer.innerHTML = "No characther with that name!";
+    cardContainer.innerHTML = "No character with that name!";
   }
   console.log("data.error", data);
   maxPage = data.info.pages;
   const characters = data.results;
   cardContainer.innerHTML = "";
-  pagination.textContent = `${page} / ${maxPage}`;
+
   characters.forEach((character) => {
     const characterCard = createCharacterCard(character);
     cardContainer.append(characterCard);
+    pagination.textContent = `${page} / ${maxPage}`;
   });
+  return data;
 }
 
 searchBar.addEventListener("submit", (event) => {
@@ -62,6 +69,9 @@ searchBar.addEventListener("submit", (event) => {
   const searchInput = event.target.elements["query"];
   searchQuery = searchInput.value;
   fetchCharacters();
+  // const data = fetchCharacters();
+  // maxPage = data.info.pages;
+
   searchBar.reset();
 });
 
